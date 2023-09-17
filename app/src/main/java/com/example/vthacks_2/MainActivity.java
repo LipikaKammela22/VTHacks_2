d1f846ed298380acf85d90c979b3e149dc081295
amaanim <110748595+amaanim@users.noreply.github.com>
2023-09-16 21:38:18 -0400

    commit am

--- app/src/main/java/com/example/vthacks_2/MainActivity.java
+++ app/src/main/java/com/example/vthacks_2/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.vthacks_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText credscore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //credscore = ;
     }
 }
